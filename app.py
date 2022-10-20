2935bbc7928bcec12f2fe6336f54c891f5e1ce2c
Areeq Hasan <areeq.i.hasan@gmail.com>
2022-10-20 18:34:15 -0400

    atom observer

---
+++ app.py
@@ -0,0 +1,112 @@
+import os
+import shutil
+
+import json
+from datetime import datetime
+
+import numpy as np
+import h5py
+
+import cv2
+from skimage.segmentation import active_contour, flood
+from skimage.filters import gaussian
+
+from flask import Flask, Response
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+frames = np.load("frames.npz")["data"]
+
+
+def stream():
+    while True:
+        for frame in frames:
+            yield frame
+
+
+def raster_stream():
+    for frame in stream():
+        _, jpeg = cv2.imencode(".jpg", (frame - 1776) / (2688 - 1776) * 255)
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + jpeg.tobytes() + b"\r\n\r\n"
+        )
+
+
+@app.route("/stream")
+def render_stream():
+    return Response(
+        raster_stream(),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
+
+
+def segment(image, roi):
+    contour = active_contour(image, roi, gamma=0.001)
+    contour_centroid = tuple(
+        np.round(np.mean(contour, axis=0)[::-1]).astype(int).tolist()
+    )
+    mask = flood(
+        gaussian(image, sigma=1.5, preserve_range=True),
+        contour_centroid[::-1],
+        tolerance=200,
+    )
+    size = np.sum(mask)
+    interior = np.where(mask > 0)
+    centroid = tuple(np.round(np.mean(interior[::-1], axis=1)).astype(int).tolist())
+    return mask, centroid, size
+
+
+@app.route("/snapshot/take")
+def take_snapshot():
+    snapshot = next(stream())
+    now = datetime.now()
+
+    with h5py.File("snapshot_tmp.hdf", "w") as tmp:
+        snapshot_ds = tmp.create_dataset("snapshot", data=snapshot)
+        snapshot_ds.attrs["timestamp"] = now.timestamp()
+
+    data = {
+        "timestamp": str(now.strftime("%x @ %X%p")),
+        "snapshot": snapshot.tolist(),
+    }
+
+    return data
+
+
+@app.route("/snapshot/segment")
+def segment_snapshot():
+    with h5py.File("snapshot_tmp.hdf", "r+") as tmp:
+        snapshot_ds = tmp["snapshot"]
+        snapshot = snapshot_ds[:]
+
+        s = np.linspace(0, 2 * np.pi, 400)
+        r = 375 + 50 * np.sin(s)
+        c = 310 + 50 * np.cos(s)
+        roi = np.array([r, c]).T
+
+        mask, centroid, size = segment(snapshot, roi)
+        segmentation_ds = tmp.create_dataset("segmentation", data=mask)
+        segmentation_ds.attrs["centroid"] = centroid
+        segmentation_ds.attrs["size"] = size
+
+    data = {
+        "mask": mask.astype(int).tolist(),
+        "centroid": list(centroid),
+        "size": int(size),
+    }
+
+    return data
+
+
+@app.route("/snapshot/store")
+def store_snapshot():
+    with h5py.File("snapshot_tmp.hdf", "r") as tmp:
+        timestamp = tmp["snapshot"].attrs["timestamp"]
+    timestamp_dt = datetime.fromtimestamp(timestamp)
+    timestamp_str = timestamp_dt.strftime("%Y-%m-%d_%H-%M-%S")
+    shutil.copy2("snapshot_tmp.hdf", f"snapshots/{timestamp_str}.hdf")
+    os.remove("snapshot_tmp.hdf")
+    return timestamp_str
---
+++ client/src/App.tsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from 'react'
+
+import Plot from 'react-plotly.js'
+
+import './App.css'
+
+interface SnapshotData {
+  timestamp: string,
+  snapshot: Array<Array<number>>,
+}
+
+interface SegmentationData {
+  mask: Array<Array<number>>,
+  centroid: Array<number>,
+  size: number
+}
+
+function App() {
+
+  const [snapshotData, setSnapshotData] = useState<SnapshotData | null>(null)
+  const [segmentationData, setSegmentationData] = useState<SegmentationData | null>(null)
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Atom Observer</h1>
+        <div className="wrapper">
+          <div className="stream">
+            <h2>Stream</h2>
+            <div>
+              <img src="http://127.0.0.1:5000/stream" style={{ width: 346, height: 521, paddingTop: 156, paddingBottom: 79 }}></img>
+              <button onClick={() => {
+                fetch("http://127.0.0.1:5000/snapshot/take").then(result => result.json())
+                  .then(
+                    (result: SnapshotData) => {
+                      setSegmentationData(null)
+                      setSnapshotData(result)
+                    }
+                  )
+              }}>Take Snapshot</button>
+            </div>
+          </div>
+          <div className="snapshot">
+            <h2>Snapshot</h2>
+            {snapshotData ?
+              <div>
+                <p>Timestamp: {snapshotData.timestamp}</p>
+                <Plot
+                  data={[
+                    {
+                      z: snapshotData.snapshot,
+                      type: 'heatmap',
+
+                    },
+                  ]}
+                  layout={{ width: 500, height: 700, yaxis: { autorange: "reversed" } }}
+                />
+                <button onClick={() => {
+                  fetch("http://127.0.0.1:5000/snapshot/segment").then(result => result.json())
+                    .then(
+                      (result: SegmentationData) => {
+                        setSegmentationData(result)
+                      }
+                    )
+                }}>Segment Snapshot</button>
+              </div>
+              : <p>
+                Once you take a snapshot, you can view the results here and configure its segmentation.
+              </p>
+            }
+          </div>
+          <div className="segmentation">
+            <h2>Segmentation</h2>
+            {segmentationData ?
+              <div>
+                <p>Centroid: ({segmentationData.centroid[0]}, {segmentationData.centroid[1]}); Size: {segmentationData.size}</p>
+                <Plot
+                  data={[
+                    {
+                      z: segmentationData.mask,
+                      type: 'heatmap',
+                    },
+                  ]}
+                  layout={{ width: 500, height: 700, yaxis: { autorange: "reversed" } }}
+                />
+                <button onClick={() => {
+                  fetch("http://127.0.0.1:5000/snapshot/store").then(() => {
+                    setSnapshotData(null)
+                    setSegmentationData(null)
+                  })
+                }}>Store Snapshot</button>
+              </div> : <p>
+                Once you segment a snapshot, you can view the results here and store the snapshot.
+              </p>
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
